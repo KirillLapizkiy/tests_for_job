65cb66aa302f1b1dad39ec63428f4107661d1d42
KirillLapizkiy <kir9lapizkiy@gmail.com>
2021-01-31 14:15:57 +0700

    Добавил возможность запуска автотестов в разных браузерах.

--- src/test/java/testProject/tests/TestBase.java
+++ src/test/java/testProject/tests/TestBase.java
@@ -1,12 +1,15 @@
 package testProject.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import testProject.appmanager.ApplicationManager;
 
 public class TestBase {
-
-    protected final ApplicationManager app = new ApplicationManager();
+    // Выбор браузера для запуска автотестов осуществляется подстановкой в BrowserType.XXX.
+    // Вебдрайверы IE и firefox лежат в репе, знаю, что это не канонично,
+    // сделал так, что бы их не искали в интернете при проверке тестового
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
