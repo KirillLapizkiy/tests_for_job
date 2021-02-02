47e2a5f2f0c408f3d336b488223b58907121c433
KirillLapizkiy <kir9lapizkiy@gmail.com>
2021-02-02 11:49:54 +0700

    Добавил предусловие наполнения вишлиста в тест по удалению позиции из вишлиста

--- src/test/java/testProject/tests/DeleteItemInWishList.java
+++ src/test/java/testProject/tests/DeleteItemInWishList.java
@@ -2,12 +2,17 @@ package testProject.tests;
 
 import com.sun.org.glassfish.gmbal.Description;
 import org.testng.annotations.Test;
+import testProject.model.SearchData;
 
 public class DeleteItemInWishList extends TestBase{
 
-    @Description("Автотест на удаление первой позиии в вишлисте.")
+    @Description("Автотест на удаление первой позиии в вишлисте, если вишлист пустой, то автотест сам наполнит его.")
     @Test
     public void testDeleteItemInWishList() {
+        app.getWishListHelper().EntenInWishList();
+        if(! app.getWishListHelper().isThereADesiredItem()){
+            app.getWishListHelper().insertItem(new SearchData("Faded Short Sleeve T-shirts"));
+        }
         app.getWishListHelper().EntenInWishList();
         app.getWishListHelper().ChoiceFirstPositionInWishList();
         app.getWishListHelper().DeleteFirstPositionFromWishList();
