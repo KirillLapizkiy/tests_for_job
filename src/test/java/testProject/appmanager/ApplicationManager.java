4c92ff97dba895e4fc7617a29804914f8b73f631
KirillLapizkiy <kir9lapizkiy@gmail.com>
2021-02-05 15:06:35 +0700

    Создал конфигурационный файл и прописал взаимодействия с ним

--- src/test/java/testProject/appmanager/ApplicationManager.java
+++ src/test/java/testProject/appmanager/ApplicationManager.java
@@ -7,9 +7,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+    private final Properties properties;
     WebDriver wd;
 
     private SessionHelper sessionHelper;
@@ -19,9 +25,13 @@ public class ApplicationManager {
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/java/testProject/resources/%s.properties", target))));
+
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
@@ -30,11 +40,11 @@ public class ApplicationManager {
             wd = new InternetExplorerDriver();
         }
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wd.get("http://automationpractice.com/index.php");
+        wd.get(properties.getProperty("web.baseUrl"));
         wishListHelper = new WishListHelper(wd);
         searchHelper = new SearchHelper(wd);
         sessionHelper = new SessionHelper(wd);
-        sessionHelper.login("kir9@sibmail.com", "GoodSecret");
+        sessionHelper.login(properties.getProperty("web.login"), properties.getProperty("web.password"));
         mainPage();
     }
 
